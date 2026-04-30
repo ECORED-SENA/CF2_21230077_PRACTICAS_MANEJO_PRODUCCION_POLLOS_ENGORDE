ad8fc8055c7ef43d84574cf4b13c94f55280268f
Henry Alvarez <halvarezast@gmail.com>
2026-04-30 07:09:21 -0500

    UpdateCFA

--- src/config/global.js
+++ src/config/global.js
@@ -173,7 +173,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/6130643_CF2_DU.pdf',
       },
       {
         icono: 'fas fa-download',
